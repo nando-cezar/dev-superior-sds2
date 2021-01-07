e4e0815b4d50c72b92208ef94337aa2d14fd2b94
nando-cezar <luisfernando_cezar@hotmail.com>
2021-01-07 16:15:15 -0300

    Front-web parte 1 finish

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,38 +1,24 @@
+import ProductList from './ProductList';
 import StepsHeader from './StepsHeader';
+import {useEffect, useState} from 'react';
 import './styles.css';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders(){
 
-    const handleSelectProduct = (product: Product) => {
-        const isAlreadySelected = selectedProducts.some(item => item.id === product.id);
-      
-        if (isAlreadySelected) {
-          const selected = selectedProducts.filter(item => item.id !== product.id);
-          setSelectedProducts(selected);
-        } else {
-          setSelectedProducts(previous => [...previous, product]);
-        }
-      }
-      
-      const handleSubmit = () => {
-        const productsIds = selectedProducts.map(({ id }) => ({ id }));
-        const payload = {
-          ...orderLocation!,
-          products: productsIds
-        }
-      
-        saveOrder(payload).then(() => {
-          toast.error('Pedido enviado com sucesso!');
-          setSelectedProducts([]);
-        })
-          .catch(() => {
-            toast.warning('Erro ao enviar pedido');
-          })
-      }
+    const[products, setProducts] = useState<Product[]>([]);
+
+    useEffect(()=>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
 
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductList products={products}/>
         </div>
     )
 }
